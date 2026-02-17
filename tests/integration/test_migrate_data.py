974bde3c136d35703b2c1394eb181f280655a00d
PierreDff <pierre.deffrennes@gmail.com>
2026-02-17 14:08:51 +0100

    Add comments

--- tests/integration/test_migrate_data.py
+++ tests/integration/test_migrate_data.py
@@ -2,6 +2,7 @@ import mongomock
 import pandas as pd
 from unittest.mock import patch
 from src.pipelines.migrate_data import MongoMigrator
+from src.utils.envconf import settings
 
 @patch('src.pipelines.migrate_data.MongoClient')
 def test_migration_logic(mock_client):
@@ -16,5 +17,7 @@ def test_migration_logic(mock_client):
     migrator.migrate(df)
     
     # Vérification que la donnée est bien "insérée"
-    inserted_doc = mock_mongo.healthcare_db.patients.find_one()
+    # On utilise settings pour récupérer dynamiquement le nom de la DB et de la collection
+    inserted_doc = mock_mongo[settings.MONGO_DB][settings.MONGO_COLLECTION].find_one()
+    assert inserted_doc is not None, "Le document n'a pas été inséré dans la base mockée"
     assert inserted_doc['name'] == 'Test User'
